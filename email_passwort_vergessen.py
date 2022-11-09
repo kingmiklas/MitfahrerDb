6c984aa3d5a334987375b47c0a8626e1db2c0e2f
Jens <jens.turi@dtvtabak.de>
2022-11-09 20:52:19 +0100

    passwort vergessen vollständig

--- email_passwort_vergessen.py
+++ email_passwort_vergessen.py
@@ -4,9 +4,22 @@ import os
 import mysql.connector
 import sys
 import random
+import smtplib, ssl
+from email.message import EmailMessage
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "mitfahrerdb@gmail.com"  # Enter your address
+#ausführendes Gerät
+
+#Windows-Computer
+password = "fqosompobdatpbpv" 
+
+#Mac
+#password = "apcmcdggunpglljy"
 
 def verbindungsaufbau():
-    cnx = mysql.connector.connect(user='root', password='', host='localhost', database='MitfahrerDB')
+    cnx = mysql.connector.connect(user='root', password='', host='localhost', database='MitfahrerDB', port = 3329)
     
     return cnx
     
@@ -20,15 +33,32 @@ def passwort_vergessen(cnx,name):
     cur = cnx.cursor()
     cur.execute(f"SELECT u.cemail FROM tuser u WHERE u.kid = {name}")
     row = cur.fetchall()
-    list = []
+    liste = []
     for i in range(len(row)):
-        list.append(str(*row[i]))
+        liste.append(str(*row[i]))
     
-    randomnumber = random_number(5)
+    randomnumber = random_number(7)
     
+    return randomnumber,liste
+    
+def send_mail(randomnumber,liste):
+    
+    receiver_email = liste
+
+    msg = EmailMessage()
+    msg.set_content(f"Ihr neues Passwaort lautet: {randomnumber}")
+    msg['Subject'] = "Passwort vergessen"
+    msg['From'] = sender_email
+    msg['To'] = receiver_email  
+    
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.send_message(msg, from_addr=sender_email, to_addrs=receiver_email)
 
 args = sys.argv[1:]
 args[0] = args[0].lower()
 
 cnx = verbindungsaufbau()
-passwort_vergessen(cnx,*args)
+randomnumber,liste = passwort_vergessen(cnx,*args)
+send_mail(randomnumber,liste)
